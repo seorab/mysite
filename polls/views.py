b5502d806cc6d2a7a19909bd5b3f75aec6c4b132
GGoReb <seorab@naver.com>
2018-11-15 18:08:35 +0900

    xxxxx

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,14 @@ from polls.models import Question
 
 
 def index(request):
+
+    # 1번 create를 이용하여 데이터 입력
+    # Question.objects.create()
+
+    # 2번 save를 이용하여 데이터 입력
+    # Question(question_text='aaaa', pub_date=timezone.now()).save()
+
+
     q = Question.objects.all()[0]
     choices = q.choice_set.all()
 
