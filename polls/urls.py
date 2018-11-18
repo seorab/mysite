9cd64ca4d7cbae22226c7519e023cb297e5e4833
GGoReb <seorab@naver.com>
2018-11-19 08:31:40 +0900

    xxxxx

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from polls.views import index
+from polls.views import index, add, detail, detail2, vote
 
 urlpatterns = [
     path('', index),
+    path('add', add),
+    path('<int:question_id>', detail,
+         name='detail'),
+    path('<int:num1>/<int:num2>', detail2),
+    path('<int:question_id>/vote', vote)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,52 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.utils import timezone
 from polls.models import Question
 
+def vote(request, question_id):
+    select = request.POST['select']
+
+    print(select)
+
+    return HttpResponse('ok')
+
+
+
+def detail(request, question_id):
+    q = Question.objects.get(id=question_id) # 조건에 맞는 데이터 1개 조회
+    c = q.choice_set.all()
+    # choice = ''
+    # for a in c:
+    #     choice += a.choice_text
+    #             request  템플릿   컨텍스트(데이터/모델)
+    return render(
+        request,
+        'polls/detail.html',
+        {
+            'question': q.question_text,
+            'choice': c
+        }
+    )
+
+    # return HttpResponse(q.question_text + '<br>' + choice)
+
+def detail2(request, num1, num2):
+    return HttpResponse(num1 + num2)
+
+def add(request):
+    q = request.GET['q']
+
+    Question.objects.create(question_text=q, pub_date=timezone.now())
+
+    return HttpResponse('')
 
 def index(request):
+
+    questions = Question.objects.all()
+    #             request       템플릿            컨텍스트 (모델/데이터)
+    return render(
+        request, 'polls/index.html',
+        {'question': questions})
 
     # 1번 create를 이용하여 데이터 입력
     # Question.objects.create()
@@ -12,14 +54,4 @@ def index(request):
     # 2번 save를 이용하여 데이터 입력
     # Question(question_text='aaaa', pub_date=timezone.now()).save()
 
-
-    q = Question.objects.all()[0]
-    choices = q.choice_set.all()
-
-    print(q.question_text)
-    print(choices[0].choice_text)
-    print(choices[1].choice_text)
-    print(choices[2].choice_text)
-
-
-    return HttpResponse('polls index')
+    # return HttpResponse('polls index')
